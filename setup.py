6c527cdf11692363b643d1a7c655919c2b6894a8
Vinicius <vjusto91@gmail.com>
2018-01-15 23:24:07 -0200

    feat(social): Add social accounts to setup

--- setup.py
+++ setup.py
@@ -30,7 +30,11 @@ if __name__ == "__main__":
     # Database
     database = ''
     while database not in ['1', '2']:
-        database = input("What is the database?\n1: POSTGRES\n2: MYSQL\n")
+        database = input(
+            "What is the database?\n"
+            "1: POSTGRES\n"
+            "2: MYSQL\n"
+        )
 
     if database == '1':
         print('Adding postgres')
@@ -43,14 +47,50 @@ if __name__ == "__main__":
 
     # Auth
     auth = ''
-    while auth not in ['1']:
-        auth = input("What is the authentication system?\n1: Rest Auth without socials\n")
+    while auth not in ['1', '2']:
+        auth = input(
+            "What is the authentication system?\n"
+            "1: Rest Auth without socials\n"
+            "2: Rest Auth with socials\n"
+        )
 
     if auth == '1':
         print('Adding rest auth without socials')
         merge_auth_branch = "git merge origin/auth --no-edit"
         os.system(merge_auth_branch)
 
+    elif auth == '2':
+        print('Adding rest auth with socials')
+        merge_auth_branch = "git merge origin/auth-social --no-edit"
+        os.system(merge_auth_branch)
+
+        provider = ''
+        while provider not in ['1', '2', '3']:
+            provider = input(
+                "What are the providers?\n"
+                "1: Facebook\n"
+                "2: Google\n"
+                "3: Facebook + Google\n"
+            )
+
+        if provider == '1':
+            print('Adding facebook provider')
+            merge_provider_branch = "git merge origin/auth-social-facebook --no-edit"
+            os.system(merge_provider_branch)
+
+        elif provider == '2':
+            print('Adding google provider')
+            merge_provider_branch = "git merge origin/auth-social-google --no-edit"
+            os.system(merge_provider_branch)
+
+        elif provider == '3':
+            print('Adding facebook and google providers')
+            merge_provider_branch = "git merge origin/auth-social-facebook-google --no-edit"
+            os.system(merge_provider_branch)
+
     print('Finalizing setup')
-    merge_dev_branch = "git checkout master && git merge dev --no-edit && rm -rf .git && rm -rf setup.py"
+    merge_dev_branch = "git checkout master && " \
+                       "git merge dev --no-edit && " \
+                       "rm -rf .git && " \
+                       "rm -rf setup.py"
     os.system(merge_dev_branch)
