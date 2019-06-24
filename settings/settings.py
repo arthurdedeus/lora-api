417676c6dab769df21dfdd1d55a783eb2018c526
Henrique Salvaro Furtado <henrique@jungledevs.com>
2019-06-24 10:23:27 -0300

    Add S3Boto3Storage as default file storage

--- settings/settings.py
+++ settings/settings.py
@@ -141,6 +141,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Storage
 ###
 if ENVIRONMENT != 'test':
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_AUTO_CREATE_BUCKET = True
     AWS_IS_GZIPPED = True
