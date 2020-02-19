d8323746bf6ac5e1b244caa4644dfe40f6d1521e
Henrique Salvaro Furtado <henrique@jungledevs.com>
2020-02-19 16:45:40 -0300

    feat: Add BaseUserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,7 @@ Accounts admin
 # Libraries
 ###
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from . import models
 
@@ -17,6 +18,10 @@ from . import models
 ###
 # Main Admin Models
 ###
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'email', 'username', 'is_active', 'last_login', 'date_joined',)
+
 @admin.register(models.ChangeEmailRequest)
 class ChangeEmailRequestAdmin(admin.ModelAdmin):
     list_display = ('email',)
