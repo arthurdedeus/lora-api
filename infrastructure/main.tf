98483d0005e22980d47e431e7e2d3cce79f07531
Nícolas Yamakoshi <nicolas@jungledevs.com>
2020-07-23 15:01:17 -0300

    fix: Fix password creation for Terraform.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -72,7 +72,7 @@ module "ecs" {
 
 resource "random_string" "password" {
   length           = 16
-  special          = true
+  special          = false
   override_special = "!#$%&*-_=+[]{}<>"
 }
 
