1e46d43ae7704ddd2723fdb93d021e0f3e2a8322
Guilherme Goes Mendonca <guigmen@gmail.com>
2020-07-29 15:31:37 -0300

    feat: Add sentry env

--- settings/settings.py
+++ settings/settings.py
@@ -265,6 +265,7 @@ if not DEBUG and ENVIRONMENT != 'test':
 
     sentry_sdk.init(
         dsn=os.environ.get('SENTRY_DSN'),
+        environment=ENVIRONMENT,
         integrations=[DjangoIntegration()],
         before_send=before_send,
     )
