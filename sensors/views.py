f6b1040b4ab4523d5f7a0c05f461bff16a8bcd7c
arthur m <arthurmd@jungledevs.com>
2021-08-19 20:25:07 -0300

    feat: Implement log creation in webhook

--- sensors/views.py
+++ sensors/views.py
@@ -1,15 +1,23 @@
 import json
 
 from django.http import HttpResponse
+from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from rest_framework import status
+
+import sensors.models as models
 
 
 # Create your views here.
 @csrf_exempt
 @require_http_methods(["POST"])
 def fucas_webhook(request):
+    def get_sensor_instance(data):
+        dev_eui = data['payload']['end_device_ids']['dev_eui']
+        sensor, created = models.Sensor.objects.get_or_create(dev_eui=dev_eui)
+        return sensor
+
     try:
         # Retrieve and clean data from request
         jsondata = request.body.decode('utf8')
@@ -19,6 +27,17 @@ def fucas_webhook(request):
             'msg': 'WEBHOOK_RECEIVED',
             'payload': str(data),
         })
+
+        # Get sensor and register log
+        sensor = get_sensor_instance(data=data)
+        decoded_payload = data['payload']['uplink_message']['decoded_payload']
+        log = models.Log.objects.create(
+            sensor=sensor,
+            timestamp=timezone.now(),
+            **decoded_payload
+        )
+
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
     except BaseException as e:
         return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
